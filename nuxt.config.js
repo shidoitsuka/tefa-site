23e6fe332bf15cbc25bade72911fbf84b46acf97
Brian Harianja <hi@shdev.tech>
2021-03-12 07:09:29 +0700

    removed unnecessary stuffs and made the header

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,30 +1,30 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - tefa-site',
-    title: 'tefa-site',
+    titleTemplate: "Teaching Factory - SKARISA",
+    title: "tefa-site",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      // prettier-ignore
+      { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" },
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -32,13 +32,13 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios"
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -46,9 +46,9 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -62,8 +62,8 @@ export default {
       }
     }
   },
+  css: ["~/assets/css/fonts"],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,63 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container fluid class="pa-0 ma-0">
+    <v-app-bar flat dense fixed class="top-bar" color="white">
+       <v-toolbar-title></v-toolbar-title>
+       <v-spacer></v-spacer>
+       <v-btn small text>Beranda</v-btn>
+       <v-btn small text>Tentang</v-btn>
+       <v-btn small text>Layanan Kami</v-btn>
+       <v-btn small depressed color="#6897FF" class="white--text">Buat Pesanan</v-btn>
+    </v-app-bar>
+
+    <v-row class="full-height first-page">
+      <v-col>
+        <h1 class="text-title">Teaching Factory</h1>
+        <h4 class="text-sub-title">part of SMK Krian 1</h4>
+      </v-col>
+      <v-col>
+        <v-img max-width="500px" max-height="500px" src="/img/header-logo.svg"></v-img>
+      </v-col>
+    </v-row>
+    
+    <v-row class="full-height second-page">
+      <v-col>
+        <v-img max-width="500px" max-height="500px" src="/img/about-us-logo.svg"></v-img>
+      </v-col>
+      <v-col>
+        <h1>About Us</h1>
+      </v-col>
+    </v-row>
+
+  </v-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
-}
+export default {};
 </script>
+
+<style lang="sass">
+*
+  // border: 1px solid red
+
+.full-height
+  height: 100vh
+
+.first-page
+  display: flex
+  justify-content: center
+  align-items: center
+
+.text-title, .text-sub-title
+  padding: 0 0 0 15rem
+
+.text-title:before
+  content: ""
+  background: url("/img/tefa-icon.svg")
+  width: 5rem
+  height: 5rem
+  left: 10rem
+  position: absolute
+
+.text-sub-title
+  font-weight: 400
+</style>
